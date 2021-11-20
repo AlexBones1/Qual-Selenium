31b3197e7937aa9da9119c507d9110dd180d1116
AlexBones1 <zergovii@yandex.ru>
2021-11-20 17:24:10 +0500

    added ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,11 @@ class LoginPageLocators():
     REGISTER_FORM_USERNAME = (By.ID, "id_registration-email")
     REGISTER_FORM_PASSWORD = (By.ID, "id_registration-password1")
     REGISTER_FORM_PASSWORD_RE = (By.ID, "id_registration-password2")
+
+
+class ProductPageLocators():
+    ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BASKET_NAME = (By.CSS_SELECTOR, ".alertinner strong")
